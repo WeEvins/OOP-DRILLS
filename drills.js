95635623dbe6a0a53bb07727069ddcfdaa00871b
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-11 10:45:48 -0500

    comit
    working on the final syntax

--- drills.js
+++ drills.js
@@ -1,13 +1,13 @@
 
 class Family {
-    constructor (name, city, age){
+    constructor(name, city, age) {
         this.name = name;
         this.city = city;
         this.age = age;
     }
-greet(){
-    console.log(`Hey! my name is ${this.name}. I am ${this.age} years old, and I live in ${this.city}.`)
-}
+    greet() {
+        console.log(`Hey! my name is ${this.name}. I am ${this.age} years old, and I live in ${this.city}.`)
+    }
 };
 
 let f1 = new Family('Adrien', 'France', 16);
@@ -26,32 +26,61 @@ f5.greet();
 
 
 // inheritence part
- class Vehicle {
-     constructor(make, model, price) {
-         this.make = make;
-         this.model = model;
-         this.price = price;
-     }
- }
+class Vehicle {
+    constructor(make, model, wheels, doors, ) {
+        this.make = make;
+        this.model = model;
+        this.wheels = wheels;
+        this.doors = doors;
 
- class Car extends Vehicle{
-     constructor(make, model, price, transmission){
-         super (make, model, price)
-         this.transmission = transmission;
-
-     }
- };
-
-
-class Bike extends Vehicle{
-    constructor(make, model, price,){
-    super (make, model, price)
-}
-};
-
-
-class Boat extends Vehicle {
-    constructor(make, model, price) {
-        super (make, model, price)
+    }
+    aboutVehicle() {
+        console.log(`I am a/an ${this.make} ${this.model}, and I have ${this.wheels} many wheels.`);
     }
 };
+
+
+// make, model, wheel#, doors#, boolean truck bed t/f, good gas mileage y/n; 
+//vehicle size s,m,l ; motorcycle has handlebars, shows property no doors;
+
+//have method aboutVehicle reflect unique features
+
+
+class Truck extends Vehicle {
+    constructor(make, model, wheels, doors, hasBed; ) {
+        super(make, model, wheels, doors)
+        this.hasBed = hasBed;
+        
+    }
+
+};
+
+class Car extends Vehicle {
+    constructor(make, model, wheels, transmission) {
+        super(make, model, wheels)
+        this.transmission = transmission;
+        this.doors = doors;
+    }
+    aboutVehicle(){
+        console.log(`I am a/an ${this.make} ${this.model}. I have ${this.wheels} wheels, and my transmission is ${this.transmission}.`);
+    }
+};
+
+class Bike extends Vehicle {
+    constructor(make, model, wheels, engineSize) {
+        super(make, model, wheels)
+        this.engineSize = engineSize;
+    }
+    
+};
+
+let car1 = new Car('audi', 'r8', 4, 'flappy');
+let car2 = new Car('fiat', '500c Abarth', 4, 'manual');
+let car3 = new Car('mazda', 'six', 4, 'automatic');
+// let bike1 = 
+// let bike2 = 
+// let bike3 =
+
+car1.aboutVehicle();
+car2.aboutVehicle();
+car3.aboutVehicle();
